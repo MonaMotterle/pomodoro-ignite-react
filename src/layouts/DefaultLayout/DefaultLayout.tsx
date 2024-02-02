18669e7d2a33f55b447855f7f9e552e105cbfd99
Monalisa de Moraes <monalisamoraeszm@gmail.com>
2024-02-02 14:21:55 -0300

    fix type and add Header component style

--- src/layouts/DefaultLayout/DefaultLayout.tsx
+++ src/layouts/DefaultLayout/DefaultLayout.tsx
@@ -1,4 +1,4 @@
-import { Header } from '../../components/Header/Header.tsx';
+import { Header } from '../../components/Header';
 import { Outlet } from 'react-router-dom';
 import { LayoutContainer } from './DefaultLayout.styles.ts';
 import { FC } from 'react';
